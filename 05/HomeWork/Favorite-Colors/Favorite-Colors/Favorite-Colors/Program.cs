f991bfede6e3475bed7545ee3b5c75dd070e0f07
JiMaC <jimac221@gmail.com>
2020-10-17 12:53:08 +0300

    no message

---
+++ 05/HomeWork/Favorite-Colors/Favorite-Colors/Favorite-Colors/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Favorite_Colors
+{
+    class Program
+    {
+        [Flags]
+        enum Colors : byte
+        {
+            None = 0x0,                 
+            Red = 0x1,                  
+            Orange = 0x1 << 1,         
+            Yellow = 0x1 << 2,          
+            Green = 0x1 << 3,           
+            Blue = 0x1 << 4,           
+            Violet = 0x1 << 5,          
+            Black = 0x1 << 6,          
+            White = 0x1 << 7           
+        }
+        static void Main(string[] args)
+        {
+            var Pallet = Colors.Red | Colors.Orange | Colors.Yellow | Colors.Green | Colors.Blue | Colors.Violet | Colors.Black | Colors.White;
+            var colorArray = (Colors[])Enum.GetValues(typeof(Colors));
+            Console.WriteLine($"Please, enter you favorite colors: ");
+       
+            for (var i = 1; i < colorArray.Length; i++)
+            {
+                Console.WriteLine($"{colorArray[i]}  ");
+            }
+        
+
+            Console.Write($"\n");
+
+            var favColors = Colors.None;
+            var unFavColors = Colors.None;
+          
+                for (var i = 1; i < 5; i++)
+                {
+                try
+                {
+                    Console.WriteLine($"\nChoose {i} color");
+                    var input = Console.ReadLine();
+                    var getValue = (Colors)Enum.Parse(typeof(Colors), input);
+                    favColors = favColors | getValue;
+                }
+
+                catch (System.ArgumentException)
+                {
+                    Console.WriteLine($"You enter wrong value!");
+                }
+
+            }
+            
+         
+            unFavColors = favColors ^ Pallet;
+            Console.WriteLine($"You favorite colors: {favColors}  ");
+            Console.WriteLine($"You don't like this colors: {unFavColors}  ");
+            Console.ReadKey();
+        }
+
+        }
+    }
+
---
+++ 05/HomeWork/Figures/Figures/Figures/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Figures
+{
+    class Program
+    {
+        enum Figures
+        {
+            Rectangle = 1,
+            Triagle, 
+            Circle
+        }
+        static void Main(string[] args)
+        {
+            var figureArray = (Figures[])Enum.GetValues(typeof(Figures));
+            Console.Write($"Please, choose figure:  ");
+
+            for (var i = 0; i < figureArray.Length; i++)
+            {
+                Console.Write($"{figureArray[i]}, ");
+            }
+            Console.WriteLine($"\n");
+            try
+            {
+                var input = Console.ReadLine();
+                var getValue = (Figures)Enum.Parse(typeof(Figures), input);
+                Console.WriteLine($"you choose:  {getValue}");
+
+                switch (Convert.ToInt32(input))
+                {
+                    case 1:
+                        Console.WriteLine("Please, enter the height of the rectangle: ");
+                        double height = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Please, enter the width of the rectangle: ");
+                        double width = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"The area of the rectangle: {height * width}");
+                        Console.WriteLine($"The perimeter of the rectangle: {2 * (height + width)}");
+                        break;
+                    case 2:
+                        Console.WriteLine("Please, enter the side of the triangle: ");
+                        double side = Convert.ToDouble(Console.ReadLine());
+                        double sqrt = Math.Sqrt(3);
+                        double squareSide = Math.Round((sqrt / 4) * (side * side), 2);
+                        Console.WriteLine($"The area of the triangle: {squareSide}");
+                        Console.WriteLine($"The perimetr of the triangle: {side * 3}");
+                        break;
+                    case 3:
+                        Console.WriteLine("Please, enter the diametr of the circle: ");
+                        double P = Math.Round(Math.PI, 2);
+                        double Diametr = Convert.ToDouble(Console.ReadLine());
+                        double area = (P / 4) * (Diametr * Diametr);
+                        Console.WriteLine($"The area of the circle: {area}");
+                        Console.WriteLine($"The perimetr of the circle: {P * Diametr}");
+                        break;
+                   default:
+                        Console.WriteLine($"Sorry, you enter the wrong value of the figure!");
+                        break;
+
+                }
+
+             }
+            catch (System.ArgumentException)
+            {
+                Console.WriteLine($"You enter wrong value!");
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine($"You enter wrong format!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
