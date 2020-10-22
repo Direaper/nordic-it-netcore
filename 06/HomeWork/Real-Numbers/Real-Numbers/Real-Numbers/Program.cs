2cbe2ee1b0fb0f4e64d55f228e28f1330c89fd46
JiMaC <jimac221@gmail.com>
2020-10-23 00:46:30 +0300

    HW done

---
+++ 06/HomeWork/Real-Numbers/Real-Numbers/Real-Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            { 
+            Console.WriteLine("Введите положительное число от 1 до 2 миллиардов");
+            int input = int.Parse(Console.ReadLine());
+            int i = 0;
+
+           while (input != 0 )
+            {
+                if ((input % 10) % 2 == 0)
+                {
+                    i++;
+                }
+                input = input / 10;
+            }
+
+
+            Console.WriteLine($"Кол-во четных цифр {i}");
+            Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели неверное значение");
+            }
+        }
+    }
+}
---
+++ 06/HomeWork/bank/bank/bank/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i == 0)
+            { 
+            try { 
+            Console.WriteLine("Введите сумму первоначального взноса в рублях: ");
+            double sumInp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Вы ввели {sumInp} рублей");
+             if (sumInp<=0)
+                    {
+                    Console.WriteLine($"Ошибка! Вы ввели отрицательное или нулевое значение. Нажмите любую клавишу, чтобы начать заново!");
+                        Console.ReadKey();
+                        Console.Clear();
+                        continue;
+                    }
+
+            Console.WriteLine("Введите ежедневный процент дохода в виде десятичной дроби (1% = 0,01):");
+            double procInp = Convert.ToDouble(Console.ReadLine());
+                    if (procInp <= 0)
+                    {
+                        Console.WriteLine($"Ошибка! Вы ввели отрицательное или нулевое значение. Нажмите любую клавишу, чтобы начать заново!");
+                        Console.ReadKey();
+                        Console.Clear();
+                        continue;
+                    }
+
+            double procValue = procInp * sumInp;
+            int day = 0;
+            Console.WriteLine($"Ваш доход будет увеличиваться на {procValue} ежедневно");
+            Console.WriteLine("Введите желаемую сумму накопления в рублях:");
+            double capInp = Convert.ToDouble(Console.ReadLine());
+                    if (capInp <= 0)
+                    {
+                        Console.WriteLine($"Ошибка! Вы ввели отрицательное или нулевое значение. Нажмите любую клавишу, чтобы начать заново!");
+                        Console.ReadKey();
+                        Console.Clear();
+                        continue;
+                    }
+                    Console.WriteLine($"Желаемая сумма накопления: {capInp} ");
+
+            while (sumInp < capInp)
+            {
+                sumInp=sumInp + procValue;
+                day=day + 1;
+
+            }
+            Console.WriteLine($"Через {day} дней, ваш доход будет: {capInp} ");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Вы ввели не верное значение!");
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine($"Введенное число превышает допустимое значение");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+            }
+        }
+    }
+}
