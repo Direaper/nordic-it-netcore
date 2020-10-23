796f36ebe663728e6d4813aaeb08a609c1995a03
JiMaC <jimac221@gmail.com>
2020-10-24 01:05:56 +0300

    переделал по правкам

--- 06/HomeWork/Real-Numbers/Real-Numbers/Real-Numbers/Program.cs
+++ 06/HomeWork/Real-Numbers/Real-Numbers/Real-Numbers/Program.cs
@@ -6,28 +6,43 @@ namespace Real_Numbers
     {
         static void Main(string[] args)
         {
-            try
-            { 
-            Console.WriteLine("Введите положительное число от 1 до 2 миллиардов");
-            int input = int.Parse(Console.ReadLine());
-            int i = 0;
-
-           while (input != 0 )
+            while (true)
             {
-                if ((input % 10) % 2 == 0)
+
+                try
                 {
-                    i++;
+                    Console.WriteLine("Введите положительное число от 1 до 2 миллиардов");
+                    int input = int.Parse(Console.ReadLine());
+                    int i = 0;
+
+                    if (input <= 2000000000)
+                    {
+                        while (input != 0)
+                        {
+                            if ((input % 10) % 2 == 0)
+                            {
+                                i++;
+                            }
+                            input = input / 10;
+                        }
+                        Console.WriteLine($"Кол-во четных цифр {i}");
+                    }
+                    else if (input < 2000000000)
+                    {
+                        Console.WriteLine($"Ошибка! Вы ввели значение больше двух миллиардов!");
+                    }
+
+                    Console.ReadKey();
                 }
-                input = input / 10;
-            }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка! Вы ввели неверное значение");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Ошибка! Вы ввели значение больше 2 миллиардов");
+                }
 
-
-            Console.WriteLine($"Кол-во четных цифр {i}");
-            Console.ReadKey();
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Вы ввели неверное значение");
             }
         }
     }
--- 06/HomeWork/bank/bank/bank/Program.cs
+++ 06/HomeWork/bank/bank/bank/Program.cs
@@ -8,21 +8,22 @@ namespace bank
         {
             int i = 0;
             while (i == 0)
-            { 
-            try { 
-            Console.WriteLine("Введите сумму первоначального взноса в рублях: ");
-            double sumInp = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"Вы ввели {sumInp} рублей");
-             if (sumInp<=0)
+            {
+                try
+                {
+                    Console.WriteLine("Введите сумму первоначального взноса в рублях: ");
+                    double sumInp = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Вы ввели {sumInp} рублей");
+                    if (sumInp <= 0)
                     {
-                    Console.WriteLine($"Ошибка! Вы ввели отрицательное или нулевое значение. Нажмите любую клавишу, чтобы начать заново!");
+                        Console.WriteLine($"Ошибка! Вы ввели отрицательное или нулевое значение. Нажмите любую клавишу, чтобы начать заново!");
                         Console.ReadKey();
                         Console.Clear();
                         continue;
                     }
 
-            Console.WriteLine("Введите ежедневный процент дохода в виде десятичной дроби (1% = 0,01):");
-            double procInp = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите ежедневный процент дохода в виде десятичной дроби (1% = 0,01):");
+                    double procInp = Convert.ToDouble(Console.ReadLine());
                     if (procInp <= 0)
                     {
                         Console.WriteLine($"Ошибка! Вы ввели отрицательное или нулевое значение. Нажмите любую клавишу, чтобы начать заново!");
@@ -31,11 +32,11 @@ namespace bank
                         continue;
                     }
 
-            double procValue = procInp * sumInp;
-            int day = 0;
-            Console.WriteLine($"Ваш доход будет увеличиваться на {procValue} ежедневно");
-            Console.WriteLine("Введите желаемую сумму накопления в рублях:");
-            double capInp = Convert.ToDouble(Console.ReadLine());
+                    double procValue = procInp * sumInp;
+                    int day = 0;
+                    Console.WriteLine($"Ваш доход будет увеличиваться на {procValue} ежедневно");
+                    Console.WriteLine("Введите желаемую сумму накопления в рублях:");
+                    double capInp = Convert.ToDouble(Console.ReadLine());
                     if (capInp <= 0)
                     {
                         Console.WriteLine($"Ошибка! Вы ввели отрицательное или нулевое значение. Нажмите любую клавишу, чтобы начать заново!");
@@ -45,25 +46,25 @@ namespace bank
                     }
                     Console.WriteLine($"Желаемая сумма накопления: {capInp} ");
 
-            while (sumInp < capInp)
-            {
-                sumInp=sumInp + procValue;
-                day=day + 1;
+                    while (sumInp < capInp)
+                    {
+                        sumInp = sumInp + procValue;
+                        day = day + 1;
 
-            }
-            Console.WriteLine($"Через {day} дней, ваш доход будет: {capInp} ");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine($"Вы ввели не верное значение!");
-            }
-            catch (System.OverflowException)
-            {
-                Console.WriteLine($"Введенное число превышает допустимое значение");
-            }
+                    }
+                    Console.WriteLine($"Через {day} дней, ваш доход будет: {capInp} ");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Вы ввели не верное значение!");
+                }
+                catch (System.OverflowException)
+                {
+                    Console.WriteLine($"Введенное число превышает допустимое значение");
+                }
 
-            Console.ReadKey();
-            Console.Clear();
+                Console.ReadKey();
+                Console.Clear();
             }
         }
     }
