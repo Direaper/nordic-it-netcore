f97e56a1c6c986f295a95ba78915a2ffa888382a
JiMaC <jimac221@gmail.com>
2020-11-07 13:16:12 +0300

    HW is done

---
+++ 11/HomeWork/Reminder/Reminder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Reminder
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTimeOffset datetime = DateTimeOffset.Parse("2020-11-07 13:41");
+            ReminderItem bud1 = new ReminderItem(datetime, "ALARM");
+            bud1.WriteProperties();
+
+            DateTimeOffset datetime2 = DateTimeOffset.Parse("2020-11-07 13:42");
+            ReminderItem bud2 = new ReminderItem(datetime2, "ALARM");
+            bud2.WriteProperties();
+        }
+    }
+}
---
+++ 11/HomeWork/Reminder/Reminder/ReminderItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Reminder
+{
+    class ReminderItem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm
+        {
+            get { return DateTimeOffset.Now - AlarmDate; }
+        }
+
+        public bool IsOutdated
+        {
+            get { return DateTime.Now > AlarmDate; }
+        }
+        public ReminderItem(DateTimeOffset alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        public void WriteProperties()
+        {
+            Console.WriteLine($"Alarm Date: {AlarmDate} \nAlarm Message: {AlarmMessage} \nTime to Alarm: {TimeToAlarm} \nIs out Date {IsOutdated} \n");
+        }
+    }
+}
